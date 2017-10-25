14981a142f3dd67ecb1e24ca6b7dc0f4866a43f5
Alecaddd <castellani.ale@gmail.com>
2017-10-24 19:22:17 -0700

    Lesson 13

---
+++ Lesson13/inc/Pages/Admin.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * @package  AlecadddPlugin
+ */
+namespace Inc\Pages;
+
+use \Inc\Base\BaseController;
+
+/**
+* 
+*/
+class Admin extends BaseController
+{
+	public function register() {
+		add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+	}
+
+	public function add_admin_pages() {
+		add_menu_page( 'Alecaddd Plugin', 'Alecaddd', 'manage_options', 'alecaddd_plugin', array( $this, 'admin_index' ), 'dashicons-store', 110 );
+	}
+
+	public function admin_index() {
+		require_once $this->plugin_path . 'templates/admin.php';
+	}
+}
