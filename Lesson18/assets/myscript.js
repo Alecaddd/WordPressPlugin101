08148a45127244d842470f9d5f23dc6ee1471e75
Alecaddd <castellani.ale@gmail.com>
2017-12-23 16:15:43 -0800

    Lesson 18

---
+++ Lesson18/assets/myscript.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", function() {
+
+	// store tabs variables
+	var tabs = document.querySelectorAll("ul.nav-tabs > li");
+
+	for (i = 0; i < tabs.length; i++) {
+		tabs[i].addEventListener("click", switchTab);
+	}
+
+	function switchTab(event) {
+		event.preventDefault();
+
+		document.querySelector("ul.nav-tabs li.active").classList.remove("active");
+		document.querySelector(".tab-pane.active").classList.remove("active");
+
+		var clickedTab = event.currentTarget;
+		var anchor = event.target;
+		var activePaneID = anchor.getAttribute("href");
+
+		clickedTab.classList.add("active");
+		document.querySelector(activePaneID).classList.add("active");
+
+	}
+
+});
