77b73a240e0a92f0fdf905227a61cd8e2b4b5872
Alecaddd <castellani.ale@gmail.com>
2018-04-16 21:41:38 -0700

    Lesson 37

---
+++ Lesson37/inc/Base/Enqueue.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * @package  AlecadddPlugin
+ */
+namespace Inc\Base;
+
+use Inc\Base\BaseController;
+
+/**
+* 
+*/
+class Enqueue extends BaseController
+{
+	public function register() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+	}
+	
+	function enqueue() {
+		// enqueue all our scripts
+		wp_enqueue_script( 'media-upload' );
+		wp_enqueue_media();
+		wp_enqueue_style( 'mypluginstyle', $this->plugin_url . 'assets/mystyle.css' );
+		wp_enqueue_script( 'mypluginscript', $this->plugin_url . 'assets/myscript.js' );
+	}
+}
