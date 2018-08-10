25da67698d2bc5cf844b20a1536d52849939746c
Alecaddd <castellani.ale@gmail.com>
2018-08-09 21:31:40 -0700

    Lesson 51

--- Lesson51/src/js/slider.js
+++ Lesson51/src/js/slider.js
@@ -1,37 +1,41 @@
-//global vars
+// global variables
 const sliderView = document.querySelector('.ac-slider--view > ul'),
     sliderViewSlides = document.querySelectorAll('.ac-slider--view__slides'),
     arrowLeft = document.querySelector('.ac-slider--arrows__left'),
     arrowRight = document.querySelector('.ac-slider--arrows__right'),
     sliderLength = sliderViewSlides.length;
 
-//slide me function
-const slideMe = function (sliderViewItems, isActiveItem, sliderActiveItem) {
+// sliding function
+const slideMe = (sliderViewItems, isActiveItem) => {
+    // update the classes
     isActiveItem.classList.remove('is-active');
-    sliderActiveItem.classList.remove('is-active');
     sliderViewItems.classList.add('is-active');
 
+    // css transform the active slide position
     sliderView.setAttribute('style', 'transform:translateX(-' + sliderViewItems.offsetLeft + 'px)');
 }
 
-const beforeSlide = function(i) {
+// before sliding function
+const beforeSliding = i => {
     let isActiveItem = document.querySelector('.ac-slider--view__slides.is-active'),
         currentItem = Array.from(sliderViewSlides).indexOf(isActiveItem) + i,
         nextItem = currentItem + i,
-        sliderViewItems = document.querySelector('.ac-slider--view__slides:nth-child(' + nextItem + ')'),
-        sliderActiveItem = document.querySelector('.ac-slider--view__slides.is-active');
+        sliderViewItems = document.querySelector(`.ac-slider--view__slides:nth-child(${nextItem})`);
 
+    // if nextItem is bigger than the # of slides
     if (nextItem > sliderLength) {
         sliderViewItems = document.querySelector('.ac-slider--view__slides:nth-child(1)');
     }
 
+    // if nextItem is 0
     if (nextItem == 0) {
-        sliderViewItems = document.querySelector('.ac-slider--view__slides:nth-child(' + sliderLength + ')');
+        sliderViewItems = document.querySelector(`.ac-slider--view__slides:nth-child(${sliderLength})`);
     }
 
-    slideMe(sliderViewItems, isActiveItem, sliderActiveItem);
+    // trigger the sliding method
+    slideMe(sliderViewItems, isActiveItem);
 }
 
-//arrow function
-arrowRight.addEventListener('click', () => beforeSlide(1));
-arrowLeft.addEventListener('click', () => beforeSlide(0));
+// triggers arrows
+arrowRight.addEventListener('click', () => beforeSliding(1));
+arrowLeft.addEventListener('click', () => beforeSliding(0));
