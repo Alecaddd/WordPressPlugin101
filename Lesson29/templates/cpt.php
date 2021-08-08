2df4041547f75b2d0a0ea50f3b6d64202b219f50
Alexander Kozlov <65955373+wpok13@users.noreply.github.com>
2021-08-08 13:54:38 +0300

    if isset but equals to 0 than must be false
    if isset but equals to 0 than must be false

--- Lesson29/templates/cpt.php
+++ Lesson29/templates/cpt.php
@@ -19,7 +19,7 @@
 				echo '<table class="cpt-table"><tr><th>ID</th><th>Singular Name</th><th>Plural Name</th><th class="text-center">Public</th><th class="text-center">Archive</th><th class="text-center">Actions</th></tr>';
 
 				foreach ($options as $option) {
-					$public = isset($option['public']) ? "TRUE" : "FALSE";
+					$public = (isset($option['public']) && $option['public'] == 1) ? "TRUE" : "FALSE";
 					$archive = isset($option['has_archive']) ? "TRUE" : "FALSE";
 
 					echo "<tr><td>{$option['post_type']}</td><td>{$option['singular_name']}</td><td>{$option['plural_name']}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\"><a href=\"#\">EDIT</a> - <a href=\"#\">DELETE</a></td></tr>";
